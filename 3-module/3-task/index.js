e01c046865a3b828650eb43ce5798f14d6c9c654
D3ikin <deikincoin@gmail.com>
2022-02-07 23:39:01 +0200

    решение задачи 3, модуль 3

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,13 @@
 function camelize(str) {
-  // ваш код...
+  return str.split('-').map(function(item, index) {
+    if (index == 0) {
+      return item;
+    } else {
+      return item[0].toUpperCase() + item.slice(1);
+    }
+  }).join('');
 }
+
+camelize('background-color') == 'backgroundColor';
+camelize('list-style-image') == 'listStyleImage';
+camelize('-webkit-transition') == 'WebkitTransition';
