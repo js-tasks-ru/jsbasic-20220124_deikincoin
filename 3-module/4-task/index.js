47db0d1af9572ac17784ff26f71bd23f846ceeba
D3ikin <deikincoin@gmail.com>
2022-02-09 00:01:03 +0200

    решение задачи 4, модуль 3

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,28 @@
-function showSalary(users, age) {
-  // ваш код...
+let user1 = {
+  "balance": "$1,825.65",
+  "picture": "https://placehold.it/32x32",
+  "age": 21,
+  "name": "Golden Branch",
+  "gender": "male",
+  "greeting": "Hello, Golden Branch! You have 7 unread messages.",
+  "favouriteFruit": "banana"
+};
+
+let usersArr = [user1];
+
+function showSalary(usersArr, age) {
+
+  infoUser = '';
+
+  usersArr.reduce(function(index, user) {
+    if(user.age <= age) {
+      return infoUser += `${user.name}, ${user.balance}\n`;
+    }
+  },0)
+  
+  return infoUser.slice('/n', -1);
 }
+
+let result = showSalary(users, 40);
+
+
