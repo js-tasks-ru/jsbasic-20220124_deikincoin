72ffb0596a3f848ac2ee633856880cbe51228b00
D3ikin <deikincoin@gmail.com>
2022-02-14 23:21:12 +0200

    решение задачи 2, модуль 5

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,16 @@
 function toggleText() {
-  // ваш код...
+  
+  let btn = document.querySelector('.toggle-text-button');
+  let text = document.querySelector('#text');
+
+  btn.addEventListener('click', toggleHide);
+
+  function toggleHide() {
+    if (text.hasAttribute('hidden')) {
+      text.removeAttribute('hidden');
+    } else {
+      text.hidden = 'true';
+    }
+  }
+
 }
